288700b07aa2dd655a5b9ffb975244c2c9f2baed
xiaoyuer <ixiaoyuer@foxmail.com>
2023-11-12 15:36:49 +0800

    Site updated: 2023-11-12 15:36:48

--- js/f12/f12.js
+++ js/f12/f12.js
@@ -1,3 +1,4 @@
-document.onkeydown = function (e) {
-    if (123 == e.keyCode || (e.ctrlKey && e.shiftKey && (74 === e.keyCode || 73 === e.keyCode || 67 === e.keyCode)) || (e.ctrlKey && 85 === e.keyCode)) return btf.snackbarShow("温馨提示：控制台已崩了!"), event.keyCode = 0, event.returnValue = !1, !1
-};
+// 若要开启f12则要同步修改layout.pug里的代码,把关于f12的代码取消注释即可
+// document.onkeydown = function (e) {
+//     if (123 == e.keyCode || (e.ctrlKey && e.shiftKey && (74 === e.keyCode || 73 === e.keyCode || 67 === e.keyCode)) || (e.ctrlKey && 85 === e.keyCode)) return btf.snackbarShow("温馨提示：控制台已崩了!"), event.keyCode = 0, event.returnValue = !1, !1
+// };
